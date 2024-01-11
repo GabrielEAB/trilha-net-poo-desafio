e4e97e3920db3f0281c2c7b204f404159c09477e
Gabriel Bezerra <gabriel.elias@ufrpe.br>
2024-01-10 22:02:00 -0300

    Projeto finalizadogit add .

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+Iphone iphone = new Iphone("987654321", "Iphone 11", "987654321", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
